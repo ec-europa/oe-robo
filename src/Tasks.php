d16fa137403134afdd84d31551efde7f875846bc
Antonio De Marco <antonio@nuvole.org>
2017-05-02 11:42:19 +0200

    #1: Use nuvoleweb/robo-config.

--- src/Tasks.php
+++ src/Tasks.php
@@ -4,11 +4,6 @@ namespace Europa\Robo;
 
 use Robo\Robo;
 use Robo\Tasks as RoboTasks;
-use Symfony\Component\Console\Input\Input;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Command\Command;
-use Robo\Config\YamlConfigLoader;
-use Robo\Config\ConfigProcessor;
 
 /**
  * Class Tasks.
@@ -18,41 +13,7 @@ use Robo\Config\ConfigProcessor;
 class Tasks extends RoboTasks {
 
   use \Boedah\Robo\Task\Drush\loadTasks;
-
-  /**
-   * Add default options.
-   *
-   * @param \Symfony\Component\Console\Command\Command $command
-   *   Command object.
-   *
-   * @hook option
-   */
-  public function defaultOptions(Command $command) {
-    $command->addOption('config', 'c', InputOption::VALUE_REQUIRED, 'Configuration file to be used instead of default `robo.yml.dist`.', 'robo.yml');
-    $command->addOption('override', 'o', InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'Configuration value(s) to be overridden. Format: "path.to.key:value"', []);
-  }
-
-  /**
-   * Command initialization.
-   *
-   * @param \Symfony\Component\Console\Input\Input $input
-   *   Input object.
-   *
-   * @hook pre-init
-   */
-  public function initializeConfiguration(Input $input) {
-    $config = Robo::config();
-    $loader = new YamlConfigLoader();
-    $processor = new ConfigProcessor();
-    $processor->extend($loader->load('robo.yml.dist'));
-    $processor->extend($loader->load($input->getOption('config')));
-    $config->import($processor->export());
-    foreach ($input->getOption('override') as $override) {
-      if (preg_match_all('/^([a-zA-z0-9\-\_\.]+):(.*)/', $override, $matches) !== FALSE) {
-        $config->set($matches[1][0], $matches[2][0]);
-      }
-    }
-  }
+  use \NuvoleWeb\Robo\Task\Config\loadTasks;
 
   /**
    * Setup Behat.
